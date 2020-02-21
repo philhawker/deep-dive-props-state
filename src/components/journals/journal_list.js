d80bf7f1bb4daf807ea3a2cfb35a2b07848b56bb
Phil Hawker <philhawker1211@gmail.com>
2020-02-21 15:46:27 -0700

    added clear button

--- src/components/journals/journal_list.js
+++ src/components/journals/journal_list.js
@@ -4,9 +4,9 @@ import { JournalEntry } from './journal_entry'
 
 const rawJournalData = [
     { title: 'Post One', content: 'Post content', status: 'draft' },
-    { title: 'Post One', content: 'Post content', status: 'draft' },
-    { title: 'Post One', content: 'Post content', status: 'draft' },
-    { title: 'Post One', content: 'Post content', status: 'draft' }
+    { title: 'Post Two', content: 'Post content', status: 'draft' },
+    { title: 'Post Three', content: 'Post content', status: 'draft' },
+    { title: 'Post Four', content: 'Post content', status: 'draft' }
 ];
 
 //class component
@@ -20,11 +20,29 @@ export default class JournalList extends Component {
             isOpens: true
         };
     }
+
+    clearEntries = () => {
+        this.setState({ journalData: [] })
+    }
+
     render() {
+        const jouranlEntries = this.state.journalData.map(journalEntry => {
+            return (
+                <div key={journalEntry.title}>
+                    <JournalEntry
+                        title={journalEntry.title}
+                        content={journalEntry.content}
+                    />
+                </div>
+            )
+        })
+
         return (
             <div>
                 <h2>{this.props.heading}</h2>
-                <JournalEntry title='some title' content='more content' />
+                {jouranlEntries}
+
+                <button onClick={this.clearEntries}>Clear Journal Entries</button>
             </div>
         );
     }
